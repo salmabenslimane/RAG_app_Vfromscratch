51fa536bd8664e9dbdb17ca8b922aa2976a435ea
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-10-31 21:17:32 +0100

    gitignore from gitignore.io

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
-#environment variables aka computer parameters that don't get pushed on repository
+#environment variables aka computer parameters that don't get sent in code
 
 import os 
 print(os.getenv('PATH'))
+
+HUGGINGFACE_API= os.getenv('HUGGINGFACE_API')
+print(HUGGINGFACE_API)
