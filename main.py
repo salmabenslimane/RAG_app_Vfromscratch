fd7b655fdc8c009d954131575433f6aeaf4974d8
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-10-31 21:11:46 +0100

    os

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ('hello world')
+from dotenv import load_dotenv
+load_dotenv()
+#environment variables aka computer parameters that don't get pushed on repository
+
+import os 
+print(os.getenv('PATH'))
