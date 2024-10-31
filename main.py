ebe8a7fe06ad29caab7b73a57d5223aa491bf4d1
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-10-31 23:10:18 +0100

    main fct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
+'''from dotenv import load_dotenv
 load_dotenv()
 #environment variables aka computer parameters that don't get sent in code
 
@@ -6,4 +6,21 @@ import os
 print(os.getenv('PATH'))
 
 HUGGINGFACE_API= os.getenv('HUGGINGFACE_API')
-print(HUGGINGFACE_API)
+print(HUGGINGFACE_API)'''
+
+import streamlit as st
+  
+
+
+
+
+def main{}:
+
+
+
+
+
+
+if __name__== '__main__':
+    main{}
+
