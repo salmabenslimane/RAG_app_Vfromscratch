4f6d7a4702d55ae3b280dd5a3ba66d6bda0785f5
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-10-31 23:18:24 +0100

    app

---
+++ app.py
@@ -0,0 +1,25 @@
+'''from dotenv import load_dotenv
+load_dotenv()
+#environment variables aka computer parameters that don't get sent in code
+
+import os 
+print(os.getenv('PATH'))
+
+HUGGINGFACE_API= os.getenv('HUGGINGFACE_API')
+print(HUGGINGFACE_API)'''
+
+import streamlit as st
+
+
+
+def main():
+   # st.set_page_config{page_title='Hi, my name is KOJO', page_icon=':books:'}
+    print("hello")
+
+
+
+
+
+if __name__== '__main__':
+    main()
+
