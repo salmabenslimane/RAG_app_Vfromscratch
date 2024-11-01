a9c099b3835db590afb2d89dbc3f77384f8f2326
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-01 23:38:27 +0100

    kojo woooorks

--- app.py
+++ app.py
@@ -4,9 +4,10 @@
 
 import streamlit as st 
 def main():
-   st.set_page_config (page_title='Hi, my name is KOJO', page_icon=':books:')
-   st.header("Your assistant for PDFs")
+   #st.set_page_config(page_title='Hi, my name is KOJO', page_icon=':books:')
+   st.header("Hi, my name is KOJO :books:")
    st.text_input("Ask about your pdf")
+
 
 
 
