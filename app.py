c9633f53f421fef6cd6381d728e6cc082d4c2dc1
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-01 23:29:17 +0100

    venv

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 
 
 
-import streamlit as st # type: ignore
+import streamlit as st 
 def main():
    st.set_page_config (page_title='Hi, my name is KOJO', page_icon=':books:')
-    
-
+   st.header("Your assistant for PDFs")
+   st.text_in
 
 
 
