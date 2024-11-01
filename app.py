436f72c32d814e2d9ef3380f9de7f6cd11dc6070
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-02 00:48:52 +0100

    update

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ import streamlit as st
 from dotenv import load_dotenv
 
 
+def get_pdf_text (PDFs):
+    Text = ''
+
 def main():
-   load_dotenv()
+   load_dotenv() #link to secrets in .env
 
    #st.set_page_config(page_title='Hi, my name is KOJO', page_icon=':books:')
    #IDK WHY THIS LINE IS NOT WORKING
@@ -20,8 +23,8 @@ def main():
        PDFs = st.file_uploader('Upload your PDFs here', accept_multiple_files=True)
        if st.button('Analyse PDFs'):
            with st.spinner("Analyzing...") #spins while loading answers 
-               # Récuperer le texte des pdfs
-
+               # Récuperer le texte des pdfs  
+                raw_text = get_pdf_text('PDFs')
                # text chunks
 
                # vector base 
