17df952665280c5f2347b067598634d493a825de
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-01 23:30:50 +0100

    push

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 def main():
    st.set_page_config (page_title='Hi, my name is KOJO', page_icon=':books:')
    st.header("Your assistant for PDFs")
-   st.text_in
+   st.text_input("Ask about your pdf")
 
 
 
