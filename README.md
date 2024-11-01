ebe2b186a1d2b3afaa3290fb487136adea759f29
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-02 00:12:36 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ChatwithPDF_Vfromscratch
 Building the pdfs chatbot from scratch with streamlit x huggingface LLM, without a starting template :) 
+
+
+Logic of the app: 
+PDFs --- Chunks of text --- Embeddings --- Embedding vector database (pinecone, faissu ...) --- Ranked results of the semantic search --- LLM --- Answer 
--- app.py
+++ app.py
@@ -4,10 +4,14 @@
 
 import streamlit as st 
 from dotenv import load_dotenv
-load_dotenv()
+
 
 def main():
+   load_dotenv()
+
    #st.set_page_config(page_title='Hi, my name is KOJO', page_icon=':books:')
+   #IDK WHY THIS LINE IS NOT WORKING
+   
    st.header("Hi, my name is KOJO :books:")
    st.text_input("Ask about your pdf")
 
