ceffd659455834d48eeba8c339a84f32c2344502
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-01 23:13:25 +0100

    main to app

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-'''from dotenv import load_dotenv
-load_dotenv()
-#environment variables aka computer parameters that don't get sent in code
-
-import os 
-print(os.getenv('PATH'))
-
-HUGGINGFACE_API= os.getenv('HUGGINGFACE_API')
-print(HUGGINGFACE_API)'''
-
-import streamlit as st
 
 
 
+
+import streamlit as st # type: ignore
 def main():
-   # st.set_page_config{page_title='Hi, my name is KOJO', page_icon=':books:'}
-    print("hello")
+   st.set_page_config (page_title='Hi, my name is KOJO', page_icon=':books:')
+    
 
 
 
