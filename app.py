4a9cdea95f61af06e2f5cf8d4cf7e0d383e2a6db
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-02 01:09:40 +0100

    afficher le texte à partir d'un pdf

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ from PyPDF2 import PdfReader
 
 
 
-def get_pdf_text (PDFs):
-   #takes a list of pdf files and returns a str with all of the content 
-    Text =''
-    for pdf in PDFS: #loop through pdfs uploaded
-        pdf_reader = PdfReader(pdf) #creates an object with pages
-        for page in pdf_reader.pages:
-            Text = page.extract_text()
-   return Text
+def get_pdf_text(PDFs): #takes a list of pdf files and returns a str with all of the content 
+   text =''
+   for pdf in PDFs: #loop through pdfs uploaded
+      pdf_reader = PdfReader(pdf) #creates an object with pages
+      for page in pdf_reader.pages:
+            text = page.extract_text()
+   return text
+
+
 
 def main():
    load_dotenv() #link to secrets in .env
@@ -27,9 +28,10 @@ def main():
        st.subheader('hhhhhhhhhhhhhhh')
        PDFs = st.file_uploader('Upload your PDFs here', accept_multiple_files=True)
        if st.button('Analyse PDFs'):
-           with st.spinner("Analyzing...") #spins while loading answers 
+           with st.spinner("Analyzing..."): #spins while loading answers
                # Récuperer le texte des pdfs  
-                raw_text = get_pdf_text('PDFs')
+                raw_text = get_pdf_text(PDFs)
+                st.write(raw_text)
                # text chunks
 
                # vector base 
