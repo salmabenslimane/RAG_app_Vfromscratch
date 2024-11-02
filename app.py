7b75da9c98111052dcb12c1bb75af1c75cd6520d
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2024-11-03 00:19:25 +0100

    update

--- app.py
+++ app.py
@@ -52,7 +52,17 @@ def get_conversation_chain(vectorstore):
 
 def handle_user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
-    st.write(response)
+    #st.write(response) to diplay 
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 def main():
    st.set_page_config(page_title='Kojo, medical assistant', page_icon="random")
